dd7fbea3cd5291980db78dd8a0e1c579d862663c
IuriiYoung <yura.young2002@gmail.com>
2024-02-02 09:44:33 +0300

    brain_games/game_seq.py

--- brain_games/game_seq.py
+++ brain_games/game_seq.py
@@ -5,7 +5,7 @@ def play_game(game_name):
     chosen_game = importlib.import_module("brain_games.games.%s" % game_name)
     import prompt
     name = prompt.string('May I have your name? ')
-    print(f'Hello,{name}!')
+    print(f'Hello, {name}')
     count = 0
     while count < 4:
         if count == 3:
