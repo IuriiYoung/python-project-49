f46190edf1b04d3e609efa6c039f96974e9e193c
Iurii_Young <yura.young2002@gmail.com>
2024-08-04 13:46:19 +0300

    Refactoring_2

--- brain_games/game_seq.py
+++ brain_games/game_seq.py
@@ -3,15 +3,15 @@
 def play_game(game):
     import prompt
     import importlib
-    game_module = importlib.import_module(".".join(("brain_games", "games", game)))
+    module = importlib.import_module(".".join(("brain_games", "games", game)))
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(game_module.DESCRIPTION)
+    print(module.DESCRIPTION)
     correct_answers = 0
 
     while correct_answers < 3:
-        question, correct_answer = game_module.create_game()
+        question, correct_answer = module.create_game()
 
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
